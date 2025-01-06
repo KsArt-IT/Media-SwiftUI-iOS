d83f1db6004a94523d171e1cc32cc4d430d4db23
KsArT-IT <ksart.it@gmail.com>
2025-01-06 22:31:52 +0200

    refactor: change MusicListScreen

--- Media-SUI/Presentation/Screens/MusicList/MusicListScreen.swift
+++ Media-SUI/Presentation/Screens/MusicList/MusicListScreen.swift
@@ -8,14 +8,53 @@
 import SwiftUI
 
 struct MusicListScreen: View {
+    @StateObject var viewModel: MusicListViewModel
+    @Binding var selected: Track?
+    
     var body: some View {
         VStack {
             TitleTextView("Music player")
-            Spacer()
+            
+            List(viewModel.tracks) { track in
+                TrackView(track: track)
+                    .onTapGesture {
+                        print("select=\(track)")
+                        selected = track
+                    }
+                    .swipeActions(edge: .trailing) {
+                        Button {
+                            withAnimation {
+                                viewModel.delete(track.localUrl)
+                            }
+                        } label: {
+                            Label("Delete", systemImage: "trash")
+                        }
+                        .tint(.red)
+                    }
+                    .swipeActions(edge: .leading) {
+                        Button {
+                            viewModel.showRename(track)
+                        } label: {
+                            Label("Rename", systemImage: "rectangle.and.pencil.and.ellipsis")
+                        }
+                        .tint(.blue)
+                    }
+            }
+            .listStyle(.plain)
+            .scrollIndicators(.hidden)
+        }
+        .frame(maxHeight: .infinity, alignment: .top)
+        .padding(.horizontal)
+        .padding(.bottom, 70)
+        .alertRename(isPresented: $viewModel.isRenameVisible, name: $viewModel.name) {
+            viewModel.rename()
+        }
+        .onAppear {
+            viewModel.updateMusicSongsList()
         }
     }
 }
 
 #Preview {
-    MusicListScreen()
+//    MusicListScreen()
 }
