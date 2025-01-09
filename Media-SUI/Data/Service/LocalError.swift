91143e16235c6e9573f98a7b5b14b0355be2e573
KsArT-IT <ksart.it@gmail.com>
2025-01-09 23:36:25 +0200

    refactor: add saveError case to LocalError for better error handling

--- Media-SUI/Data/Service/LocalError.swift
+++ Media-SUI/Data/Service/LocalError.swift
@@ -9,12 +9,15 @@ import Foundation
 
 enum LocalError: Error {
     case directoryError(String)
+    case saveError(String)
     
     // MARK: - Description
     var localizedDescription: String {
         switch self {
         case .directoryError(let dir):
             String(localized: "Directory error: \(dir)")
+        case .saveError(let file):
+            String(localized: "File creating error: \(file)")
         }
     }
 }
