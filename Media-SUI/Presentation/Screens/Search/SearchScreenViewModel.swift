36b6bae0cff77e9156589235165376b19d1ae08e
KsArT-IT <ksart.it@gmail.com>
2025-01-11 22:53:53 +0200

    refactor: enhance SearchScreenViewModel with improved task management and local track handling

--- Media-SUI/Presentation/Screens/Search/SearchScreenViewModel.swift
+++ Media-SUI/Presentation/Screens/Search/SearchScreenViewModel.swift
@@ -16,41 +16,55 @@ final class SearchScreenViewModel: ObservableObject {
     private var page = 0
     @Published var tracks: [Track] = []
     @Published var currentTrack: Track?
-    private var task: Task<Void, Never>?
+    private var taskFetch: Task<Void, Never>?
     private var taskDownload: Task<Void, Never>?
     
     init(repository: MusicRepository, localRepository: LocalRepository) {
         self.repository = repository
         self.localRepository = localRepository
-        
-//        loadTracksFirst()
     }
     
+    // MARK: - Download
     public func download(_ track: Track) {
-        guard taskDownload == nil else { return }
+        guard taskDownload == nil, track.localUrl == nil else { return }
         
-        taskDownload = Task { [weak self] in
+        let task = Task { [weak self] in
             print("SearchScreenViewModel: \(#function) file: \(track.name)")
             await self?.setState(.loading)
-            await self?.downloadTrack(track)
+            // проверить, может файл уже был загружен
+            let fileUrl: URL?
+            if let url = await self?.getFileLocalUrl(track) {
+                fileUrl = url
+            } else {
+                // загружаем
+                if let data = await self?.downloadTrack(track),
+                   let url = await self?.saveTrack(name: track.name, data: data) {
+                    fileUrl = url
+                } else {
+                    fileUrl = nil
+                }
+            }
+            if let fileUrl {
+                // обновим ссылку на файл
+                await self?.setTrack(track.copy(name: fileUrl.lastPathComponent, url: fileUrl))
+            }
+            await self?.setState(.reload)
+
             self?.taskDownload = nil
         }
+        self.taskDownload = task
     }
     
-    private func downloadTrack(_ track: Track) async {
-        print()
-        print("SearchScreenViewModel: \(#function)")
-        let result = await repository.fetchTrack(url: track.audiodownload)
+    // MARK: - Local Tracks URL
+    private func getFileLocalUrl(_ track: Track) async -> URL? {
+        print("SearchScreenViewModel: \(#function) file: \(track.name)")
+        let result = await localRepository.fetchTrackUrl(by: track.name)
         
-        switch result {
-        case .success(let data):
-            if let url = await saveTrack(name: track.name, data: data) {
-                print("SearchScreenViewModel: \(#function) file: \(url)")
-                await setTrack(track.copy(name: url.lastPathComponent, url: url))
-            }
-            await setState(.reload)
-        case .failure(let error):
-            await showError(error)
+        return switch result {
+        case .success(let url):
+            url
+        case .failure(_):
+            nil
         }
     }
     
@@ -58,24 +72,37 @@ final class SearchScreenViewModel: ObservableObject {
         let result = await localRepository.saveTrack(name: name, data: data)
         print("SearchScreenViewModel: \(#function)")
 
-        switch result {
+        return switch result {
         case .success(let url):
-            print("SearchScreenViewModel: \(#function) file: \(url)")
-            return url
+            url
         case .failure(let error):
-            print("SearchScreenViewModel: \(#function) error: \(error)")
-            return nil
+            nil
         }
     }
     
+    // MARK: - Download Tracks Name
+    private func downloadTrack(_ track: Track) async -> Data? {
+        print("\nSearchScreenViewModel: \(#function)")
+        let result = await repository.fetchTrack(url: track.audiodownload)
+        
+        switch result {
+        case .success(let data):
+            return data
+        case .failure(let error):
+            await showError(error)
+        }
+        return nil
+    }
+    
     public func loadTracksFirst() {
-        guard task == nil else { return }
+        guard taskFetch == nil else { return }
         
         print("SearchScreenViewModel: \(#function)")
-        task = Task { [weak self] in
+        let task = Task { [weak self] in
             await self?.loadTracks()
-            self?.task = nil
+            self?.taskFetch = nil
         }
+        self.taskFetch = task
     }
     
     private func loadTracks() async {
@@ -92,6 +119,7 @@ final class SearchScreenViewModel: ObservableObject {
         }
     }
     
+    // MARK: - Set Tracks
     @MainActor
     private func setTracks(_ tracks: [Track]) async {
         print("SearchScreenViewModel: \(#function)")
@@ -108,16 +136,18 @@ final class SearchScreenViewModel: ObservableObject {
     
     @MainActor
     private func setTrack(_ track: Track) async {
-        print("SearchScreenViewModel: \(#function)")
+        print("SearchScreenViewModel: \(#function) file: \(track.localUrl?.absoluteString ?? "")")
         guard let index = self.tracks.firstIndex(where: { $0.id == track.id }) else { return }
         self.tracks[index] = track
     }
     
+    // MARK: - Set State
     @MainActor
     private func setState(_ state: ReloadingState) async {
         tracksState = state
     }
     
+    // MARK: - Show Errors
     @MainActor
     private func showError(_ error: Error) async {
         let message = if let error = error as? NetworkError {
