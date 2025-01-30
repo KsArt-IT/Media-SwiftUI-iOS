49b671416faac3611f591d54324c9ca25c17e082
KsArT-IT <ksart.it@gmail.com>
2025-01-30 17:25:50 +0200

    refactor: replace MusicInfoView with PlayerMinView in CustomBottomSheet for improved UI

--- Media-SUI/Presentation/Screens/Player/BottomSheet/CustomBottomSheet.swift
+++ Media-SUI/Presentation/Screens/Player/BottomSheet/CustomBottomSheet.swift
@@ -8,39 +8,32 @@
 import SwiftUI
 
 struct CustomBottomSheet: View {
-    @Binding var expand: Bool
+    var expand: Bool
     var animation: Namespace.ID
     let state: TrackState?
     let action: (PlayerAction) -> Void
-
+    
     var body: some View {
         // Animating Sheet Background (To Look Like It`s Expanding From the Bottom)
-        ZStack {
-            if expand {
-                Rectangle()
-                    .fill(.clear)
-            } else {
-                Rectangle()
-                    .fill(.ultraThickMaterial)
-                    .overlay {
-                        MusicInfoView(expandSheet: $expand, animation: animation, state: state, action: action)
-                    }
-                    .matchedGeometryEffect(id: Constants.bgGeometryEffectId, in: animation)
-            }
-        }
-        .frame(height: 70)
-        // Separator Line
-        .overlay(alignment: .bottom, content: {
-            Rectangle()
-                .fill(.gray.opacity(0.3))
-                .frame(height: 1)
-                .offset(y: -5)
-        })
-        // 49: Default Tab Bar Height
-        .offset(y: -49)
-    }
+        Rectangle()
+            .fill(.ultraThickMaterial)
+            .overlay {
+                PlayerMinView(animation: animation, state: state, action: action)
+                    .foregroundStyle(Color.primary)
+                    .padding(.horizontal)
+                    .padding(.top, expand ? 10 : 2)
+                    .padding(.bottom, 6)
 }
-
-#Preview {
-//    CustomBottomSheet()
+            .matchedGeometryEffect(id: Constants.bgGeometryEffectId, in: animation)
+            .frame(height: 70)
+        // Separator Line
+            .overlay(alignment: .bottom, content: {
+                Rectangle()
+                    .fill(.gray.opacity(0.3))
+                    .frame(height: 1)
+                    .offset(y: -2)
+            })
+        // 49: Default Tab Bar Height
+            .offset(y: -49)
+    }
 }
